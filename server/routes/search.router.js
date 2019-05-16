d7b96b424a13d1ec95cb344639304cadb0708c90
Meyer <ehlmeyer@gmail.com>
2019-05-16 14:01:39 -0500

    slight changes to the search.router

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -4,7 +4,9 @@ const axios = require('axios');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-    let url=`http://api.giphy.com/v1/gifs/search?api_key=${process.GIPHY_API_KEY}&tag=${req.query.tag}`
+    console.log('in /search GET', req.query);
+    
+    let url=`http://api.giphy.com/v1/gifs/search?api_key=${process.GIPHY_API_KEY}&q=${req.query.tag}`
     axios.get(url)
     .then(response => {
         console.log(response.data);
