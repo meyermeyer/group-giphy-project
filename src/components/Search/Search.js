8dfeb1299ced0815c5086a2334acdd16fe652f37
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-05-16 13:37:10 -0500

    made search form and functions, local states, and displayDiv

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,48 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Button, TextField} from '@material-ui/core';
 
 class Search extends Component {
+
+	state = {
+		favorited: [],
+		search: ''
+	}
+
+	handleChange = (event) => {
+		// capture search term entered into input and set as local search term state
+		this.setState({
+			search: event.target.value
+		})
+	};//end handleChange
+
+	searchSubmit = event => {
+		event.preventDefault();
+		console.log('submit search!')
+	};//end searchSubmit
+
 	render(){
+		console.log('search:', this.state.search);
 		return(
-			<p>HERE IS THE SEARCH</p>
+			<div className="searchDiv">
+				<form className="searchForm" onSubmit={this.searchSubmit} >
+					<TextField
+						onChange={this.handleChange} id="outlined-name" margin="normal"
+						variant="outlined" label="Search for a gif!" placeholder="What gifs you want?"/>
+						<br></br>
+					<Button type="submit" variant="contained" 
+						color="primary">Search!</Button>
+				</form>
+
+				<div className="displaySearchDiv">
+					<p>GIFS GO HERE</p>
+					{/* {this.props.reduxState.displayResults.map((gif, i) => {
+						return(
+							<img key={i} src={this.props.reduxState.displayResults.gif.PATH} alt="id?"/>
+						)
+					})} */}
+				</div>
+			</div>
 		)
 	}
 }
