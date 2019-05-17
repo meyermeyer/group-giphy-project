70782c1f54459b0ace7075205f2729f3e362c092
Meyer <ehlmeyer@gmail.com>
2019-05-17 10:02:42 -0500

    updated category name

--- database.sql
+++ database.sql
@@ -11,7 +11,7 @@ CREATE TABLE "category" (
 
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
-VALUES ('funny'), ('vega'), ('cartoon'), ('nsfw'), ('meme');
+VALUES ('funny'), ('baconian'), ('cartoon'), ('nsfw'), ('meme');
 
 -- I made our favorites table
 CREATE TABLE "favorites" (
